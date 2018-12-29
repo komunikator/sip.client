54d127acca2720ff107acb03f432e3a1b3c44a57
aea <aea@digt.ru>
2018-12-29 09:47:20 +0000

    Added tests double sended message and double call

--- test/test-start-sip-server.js
+++ test/test-start-sip-server.js
@@ -60,6 +60,107 @@ describe('Send Message Tests', function() {
     });
 
 
+    it('Send Message 2xWS <- WS', function(done) {
+        this.timeout(2000);
+
+        let uaAlice;
+
+        let ua1 = new SIP.UA({
+            //uri: 'sip:1@127.0.0.1',
+            uri: 'sip:1@127.0.0.1',
+            user: '1',
+            password: '1',
+            wsServers: ['ws://127.0.0.1:8506'],
+            //wsServers: ['udp://127.0.0.1:5060'],
+            //wsServers: ['tcp://127.0.0.1:5060'],
+            //wsServers: ['tls://127.0.0.1:5061'],
+            register: true,
+            // mediaHandlerFactory: SIP.RTP.MediaHandler.defaultFactory,
+            //mediaHandlerFactory: SIP.WebRTC.MediaHandler.defaultFactory,
+            registerExpires: 120,
+            transport: 'ws'
+                //transport: 'udp'
+                //transport: 'tcp'
+                //transport: 'tls'
+        });
+
+        ua1.on('registered', function() {
+            let ua11 = new SIP.UA({
+                //uri: 'sip:1@127.0.0.1',
+                uri: 'sip:1@127.0.0.1',
+                user: '1',
+                password: '1',
+                wsServers: ['ws://127.0.0.1:8506'],
+                //wsServers: ['udp://127.0.0.1:5060'],
+                //wsServers: ['tcp://127.0.0.1:5060'],
+                //wsServers: ['tls://127.0.0.1:5061'],
+                register: true,
+                // mediaHandlerFactory: SIP.RTP.MediaHandler.defaultFactory,
+                //mediaHandlerFactory: SIP.WebRTC.MediaHandler.defaultFactory,
+                registerExpires: 120,
+                transport: 'ws'
+                    //transport: 'udp'
+                    //transport: 'tcp'
+                    //transport: 'tls'
+            });
+
+            ua11.on('registered', function() {
+                startAlice();
+            });
+
+            ua11.on('message', function(msg) {
+            });
+
+            
+        });
+
+        ua1.on('message', function(msg) {
+            ua1.unregister();
+            uaAlice.unregister();
+
+            if (msg.body == 'Hello Bob!') {
+                setTimeout(function() {
+                    done();
+                }, 1000);
+            } else {
+                done('Message not Hello Bob!');
+            }
+        });
+
+        ua1.start();
+
+        function startAlice() {
+            uaAlice = new SIP.UA({
+                //uri: 'sip:1@127.0.0.1',
+                uri: 'sip:alice@127.0.0.1',
+                user: 'alice',
+                password: 'alice',
+                wsServers: ['ws://127.0.0.1:8506'],
+                //wsServers: ['udp://127.0.0.1:5060'],
+                //wsServers: ['tcp://127.0.0.1:5060'],
+                //wsServers: ['tls://127.0.0.1:5061'],
+                register: true,
+                // mediaHandlerFactory: SIP.RTP.MediaHandler.defaultFactory,
+                //mediaHandlerFactory: SIP.WebRTC.MediaHandler.defaultFactory,
+                registerExpires: 120,
+                transport: 'ws'
+                    //transport: 'udp'
+                    //transport: 'tcp'
+                    //transport: 'tls'
+            });
+
+            uaAlice.on('registered', function() {
+                function sendMessageAccountTo1() {
+                    let text = 'Hello Bob!';
+                    uaAlice.message('sip:1@127.0.0.1', text);
+                }
+                sendMessageAccountTo1();
+            });
+
+            uaAlice.start();
+        }
+    });
+
     it('Send Message WS <- WS', function(done) {
         this.timeout(2000);
 
@@ -1362,5 +1463,145 @@ describe('Call Tests', function() {
             file.pipe(reader);
         });
 
+    });
+
+    it('Call 2xUDP <- UDP', function(done) {
+        this.timeout(300000);
+
+        let ua1 = new SIP.UA({
+            uri: 'sip:1@127.0.0.1',
+            user: '1',
+            password: '1',
+            //wsServers: ['ws://127.0.0.1:8506'],
+            wsServers: ['udp://127.0.0.1:5060'],
+            //wsServers: ['tcp://127.0.0.1:5060'],
+            //wsServers: ['tls://127.0.0.1:5061'],
+            register: true,
+            mediaHandlerFactory: SIP.RTP.MediaHandler.defaultFactory,
+            //mediaHandlerFactory: SIP.WebRTC.MediaHandler.defaultFactory,
+            registerExpires: 120,
+            //transport: 'ws'
+            transport: 'udp'
+                //transport: 'tcp'
+                //transport: 'tls'
+        });
+
+        let logger = ua1.getLogger('test');
+
+        setTimeout(function() {
+            let fs = require('fs');
+            let wav = require('wav');
+            let fileName = 'media/Добро_пожаловать_в демонстрацию_системы_MARS.wav';
+
+            // Передача файла по rtp
+            let file = fs.createReadStream(fileName);
+            let reader = new wav.Reader();
+
+            reader.on('format', function(format) {
+                let options = {
+                    media: {
+                        stream: reader
+                    }
+                };
+
+                let session = ua1.invite('sip:alice@127.0.0.1', options);
+            });
+            file.pipe(reader);
+        }, 2000);
+
+        let uaAlice = new SIP.UA({
+            uri: 'sip:alice@127.0.0.1',
+            user: 'alice',
+            password: 'alice',
+            //wsServers: ['ws://127.0.0.1:8506'],
+            wsServers: ['udp://127.0.0.1:5060'],
+            //wsServers: ['tcp://127.0.0.1:5060'],
+            //wsServers: ['tls://127.0.0.1:5061'],
+            register: true,
+            mediaHandlerFactory: SIP.RTP.MediaHandler.defaultFactory,
+            //mediaHandlerFactory: SIP.WebRTC.MediaHandler.defaultFactory,
+            registerExpires: 120,
+            //transport: 'ws'
+            transport: 'udp'
+                //transport: 'tcp'
+                //transport: 'tls'
+        });
+
+        uaAlice.on('invite', function(session) {
+            let fs = require('fs');
+            let wav = require('wav');
+            let fileName = 'media/Спасибо_за_оценку.wav';
+
+            // Передача файла по rtp
+            let file = fs.createReadStream(fileName);
+            let reader = new wav.Reader();
+
+            reader.on('format', function(format) {
+                let options = {
+                    media: {
+                        stream: reader
+                    }
+                };
+
+                session.accept(options);
+
+                let fileNameRemoteStream = 'rec/remoteStream.raw';
+                let remoteStream = session.getRemoteStreams();
+                let writeStream = fs.createWriteStream(fileNameRemoteStream);
+
+                remoteStream.on('data', (data) => {
+                    writeStream.write(data);
+                });
+
+                // Проверка на корректность переданных данных
+                setTimeout(() => {
+                    let remoteStream = fs.readFileSync(fileNameRemoteStream);
+                    let readStream = fs.createReadStream('media/Добро_пожаловать_в демонстрацию_системы_MARS.wav');
+                    let wavReader = new wav.Reader();
+
+                    wavReader.on('format', function(format) {
+                        wavReader.on('data', (data) => {
+                            remoteStream = remoteStream.slice(1, data.length + 1);
+
+                            function isEqualBuffers() {
+                                for (let i = 0, len = data.length; i < len; i++) {
+                                    if (data[i] != remoteStream[i]) {
+                                        return false;
+                                    }
+                                }
+                                return true;
+                            }
+
+                            if (isEqualBuffers()) {
+                                done();
+                            } else {
+                                done('Buffer are not identical');
+                            }
+                        });
+                    });
+                    readStream.pipe(wavReader);
+                }, 6000);
+
+            });
+            file.pipe(reader);
+        });
+
+        let uaAlice1 = new SIP.UA({
+            uri: 'sip:alice@127.0.0.1',
+            user: 'alice',
+            password: 'alice',
+            //wsServers: ['ws://127.0.0.1:8506'],
+            wsServers: ['udp://127.0.0.1:5060'],
+            //wsServers: ['tcp://127.0.0.1:5060'],
+            //wsServers: ['tls://127.0.0.1:5061'],
+            register: true,
+            mediaHandlerFactory: SIP.RTP.MediaHandler.defaultFactory,
+            //mediaHandlerFactory: SIP.WebRTC.MediaHandler.defaultFactory,
+            registerExpires: 120,
+            //transport: 'ws'
+            transport: 'udp'
+                //transport: 'tcp'
+                //transport: 'tls'
+        });
     });
 });
